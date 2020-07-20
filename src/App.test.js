0f1a85777ca10a0f808237a14610565ce2227f44
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-20 18:42:39 -0500

    🐛 FIX: App checks if 'fetching data' shows when App has no show data

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("App renders without errors", () => {
+test("App without shows renders 'fetching data' without errors", () => {
   render(<App />);
   // screen.debug();
+  expect(screen.getByText(/fetching data/i)).toBeInTheDocument();
 });
